2d1236afc5a663a9b92e5941a766fd76738ccd85
votanloccntt <votanloccntt@gmail.com>
2023-07-14 19:12:48 +0700

    setup and add navbar UI

---
+++ src/app/components/navbar/Logo.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
+const Logo = () => {
+  const router = useRouter();
+  return (
+    <Image
+      height="100"
+      width="100"
+      alt="Logo"
+      className="hidden md:block cursor-pointer"
+      src="/logo.png"
+    />
+  );
+};
+
+export default Logo;
