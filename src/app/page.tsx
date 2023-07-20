3646c9008394dfd801be6a7db4343b7d1a462375
votanloccntt <votanloccntt@gmail.com>
2023-07-20 19:23:28 +0700

    individual View

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ const Home = async () => {
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-rows-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing: any) => {
+          {listings.map((listing) => {
             return (
               <ListingCard
                 currentUser={currentUser}
