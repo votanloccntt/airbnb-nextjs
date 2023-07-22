ce4fae36e8cc421e23da7a61b4eb7225ce2d7aa4
votanloccntt <votanloccntt@gmail.com>
2023-07-22 17:06:57 +0700

    fix user github

--- src/app/actions/getCurrentUser.ts
+++ src/app/actions/getCurrentUser.ts
@@ -1,5 +1,6 @@
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth/next";
+
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import prisma from "@/app/libs/prismadb";
 
 export async function getSession() {
@@ -9,10 +10,12 @@ export async function getSession() {
 export default async function getCurrentUser() {
   try {
     const session = await getSession();
+
     if (!session?.user?.email) {
       return null;
     }
-    const currentUser = await prisma?.user.findUnique({
+
+    const currentUser = await prisma.user.findUnique({
       where: {
         email: session.user.email as string,
       },
