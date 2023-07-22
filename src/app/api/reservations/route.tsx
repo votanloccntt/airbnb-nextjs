87948c71bc50a8698eb2c532f963a336952c3417
votanloccntt <votanloccntt@gmail.com>
2023-07-22 15:32:54 +0700

    fix post fail

--- src/app/api/reservations/route.tsx
+++ src/app/api/reservations/route.tsx
@@ -1,5 +1,7 @@
-import getCurrentUser from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
+
+import prisma from "@/app/libs/prismadb";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
 export async function POST(request: Request) {
   const currentUser = await getCurrentUser();
@@ -9,14 +11,13 @@ export async function POST(request: Request) {
   }
 
   const body = await request.json();
-
   const { listingId, startDate, endDate, totalPrice } = body;
 
   if (!listingId || !startDate || !endDate || !totalPrice) {
     return NextResponse.error();
   }
 
-  const listingAndReservation = await prisma?.listing.update({
+  const listingAndReservation = await prisma.listing.update({
     where: {
       id: listingId,
     },
