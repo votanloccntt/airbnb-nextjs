34466c0e95fe06e1a1ec2ae0057aaa4fd6e3fcba
votanloccntt <votanloccntt@gmail.com>
2023-07-22 15:21:26 +0700

    fix

--- src/app/listings/[listingId]/page.tsx
+++ src/app/listings/[listingId]/page.tsx
@@ -1,9 +1,11 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById";
+import getReservations from "@/app/actions/getReservations";
+
 import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
+
 import ListingClient from "./ListingClient";
-import getReservations from "@/app/actions/getReservations";
 
 interface IParams {
   listingId?: string;
@@ -14,12 +16,13 @@ const ListingPage = async ({ params }: { params: IParams }) => {
   const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
 
-  if (!listing)
+  if (!listing) {
     return (
       <ClientOnly>
         <EmptyState />
       </ClientOnly>
     );
+  }
 
   return (
     <ClientOnly>
